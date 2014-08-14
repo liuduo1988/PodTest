cc712c9c65e20cf635ea235df4e7f6fb09905469
liuduo <liuduo@hujiang.com>
2014-08-14 10:12:50 +0800

    Release 1.0.0.

---
+++ PodTest/PodTest.m
@@ -0,0 +1,13 @@
+//
+//  PodTest.m
+//  PodTest
+//
+//  Created by 刘铎 on 14-8-14.
+//  Copyright (c) 2014年 Shanghai Hujia Cultural Communications Co.,Ltd. All rights reserved.
+//
+
+#import "PodTest.h"
+
+@implementation PodTest
+
+@end
